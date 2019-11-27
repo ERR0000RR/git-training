d98f2a6732fc2c5443a0f173f4e9d6ce105b6cef
err0000rr <post@erikroed.no>
2019-11-27 23:23:44 +0100

    Add task2

---
+++ src/main/java/no/erikroed/git/task2/Task2.java
@@ -0,0 +1,19 @@
+package no.erikroed.git.task2;
+
+public class Task2 {
+
+    /**
+     * Revert commit - https://www.atlassian.com/git/tutorials/undoing-changes/git-revert
+     * 1) Create a constructor in this file that prints out "Bob is building..." to the console
+     * 2) Commit this change with following commit message "Add constructor to Task2"
+     * 3) Create a private method called startBuild() that takes a name of type String as input parameter
+     * and should print "<name> is building..." to the console
+     * 4) Change the constructor to call this method and use your name as argument (or keep using Bob)
+     * 5) Commit this changes with the message "Add startBuild method"
+     * 6) Push this commit and realize that this was unnecessary
+     * 7) Revert the latest commit and make sure the commit message for the revert is Revert "Add startBuild method"
+     * 8) Push the revert
+     * 9) Run the Task2Test and check if you did it correctly -> test == true ? checkoutTask3 : finishTask2
+     */
+
+}
