b4a085edf04f32c8ae726944c59e09c2f9a05c83
err0000rr <post@erikroed.no>
2019-11-27 22:09:36 +0100

    Add task1

---
+++ src/main/java/no/erikroed/git/task1/Task1.java
@@ -0,0 +1,15 @@
+package no.erikroed.git.task1;
+
+public class Task1 {
+
+    /**
+     * Cherry-pick commit - https://www.atlassian.com/git/tutorials/cherry-pick
+     * 1) Make a new branch (use your imaginary and call it whatever you want, example: feature/constructor)
+     * 2) Create a constructor in this file that prints out "Bob is building..." to the console
+     * 3) Commit this change with following commit message "Add constructor to Task1"
+     * 4) Checkout the task1 branch
+     * 5) In your preferred way, cherry-pick the commit from the branch you created in step 1 to this branch
+     * 6) Run the Task1Test and check if you did it correctly -> test == true ? checkoutTask2 : finishTask1
+     */
+
+}
