7b5dc9d9ed95d04335748a4fbafd3cde2424dda3
err0000rr <post@erikroed.no>
2019-12-05 23:38:58 +0100

    Add beginning of task3

---
+++ src/main/java/no/erikroed/git/task3/Task3.java
@@ -0,0 +1,22 @@
+package no.erikroed.git.task3;
+
+public class Task3 {
+
+    /**
+     * Rest - https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+     * Disclaimer: This task will rewrite history, be sure to know what you are doing if you use this command in
+     * projects where you collaborate with other people like bob
+     *
+     *  1) Create a constructor in this file that prints out "Bob is building..." to the console
+     * 2) Commit this change with following commit message "Add constructor to Task3"
+     * 3) Create a private method called startBuild() that takes a name of type String as input parameter
+     * and should print "<name> is building..." to the console
+     * 4) Change the constructor to call this method and use your name as argument (or keep using Bob)
+     * 5) Commit this changes with the message "Add startBuild method"
+     * 6)
+     * 7)
+     * 8)
+     * 9) Run the Task2Test and check if you did it correctly -> test == true ? checkoutTask3 : finishTask2
+     */
+
+}
