8f8a9158dcca5002ffd0d17db0d7a9713422d0d6
err0000rr <post@erikroed.no>
2019-12-05 23:54:17 +0100

    Finish task3 description

--- src/main/java/no/erikroed/git/task3/Task3.java
+++ src/main/java/no/erikroed/git/task3/Task3.java
@@ -4,8 +4,8 @@ public class Task3 {
 
     /**
      * Rest - https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
-     * Disclaimer: This task will rewrite history, be sure to know what you are doing if you use this command in
-     * projects where you collaborate with other people like bob
+     * Disclaimer: This task will erase history, be sure to know what you are doing if you use this command in
+     * projects where you collaborate with other developers(?) like Bob.
      *
      *  1) Create a constructor in this file that prints out "Bob is building..." to the console
      * 2) Commit this change with following commit message "Add constructor to Task3"
@@ -13,10 +13,10 @@ public class Task3 {
      * and should print "<name> is building..." to the console
      * 4) Change the constructor to call this method and use your name as argument (or keep using Bob)
      * 5) Commit this changes with the message "Add startBuild method"
-     * 6)
-     * 7)
-     * 8)
-     * 9) Run the Task2Test and check if you did it correctly -> test == true ? checkoutTask3 : finishTask2
+     * 6) Push all the changes to remote repository
+     * 7) Oh no... Bob was not ready for this, let's please Bob by remove this commits with the reset functionality.
+     * 8) Do reset with --hard flag and force push this changes (if you don't force this push, the commits will return)
+     * 9) Run the Task3Test and check if you did it correctly -> test == true ? waitForMoreTasksOrContribute : finishTask3
      */
 
 }
